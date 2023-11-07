c98b1c33e414381e3b6c44bd948bd87a0e7a8a99
jluo <jluo8@wgu.edu>
2023-11-06 23:30:15 -0800

    update

--- my-frontend/src/components/Footer.js
+++ my-frontend/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Github } from "react-bootstrap-icons";
+import { FaSquareGitlab } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -9,8 +10,13 @@ const Footer = () => {
       className=" myFooter navbar  py-3 fixed-bottom shadow-lg justify-content-center container-fluid ">
       {" "}
       Copyright &copy; {new Date().getFullYear()} PurpleCat PC Store{" "}
-      <a href="" target="_blank">
+      {/* <a href="https://github.com/jluo-github" target="_blank">
         <Github className="myIcon" width="100" height="30" />
+      </a> */}
+      <a
+        href="https://gitlab.com/wgu-gitlab-environment/student-repos/jluo8/d424-software-engineering-capstone/-/tree/working_branch"
+        target="_blank">
+        <FaSquareGitlab className="myIcon ms-5" size={30} />
       </a>
     </footer>
   );
